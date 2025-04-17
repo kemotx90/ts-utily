94fa6f322223484ba1527eaa88f5aa84e7e306df
048200987 <daniele.carlini-ext@movyon.com>
2025-04-17 18:08:46 +0200

    add undefined allowed for nested key

--- src/model/nested-key-of.ts
+++ src/model/nested-key-of.ts
@@ -1,9 +1,10 @@
 /**
  * for nested object into array use object.array.0.nested (no ide suggestion as propose)
  */
-export type NestedKeyOf<ObjectType extends object> =
+export type NestedKeyOf<ObjectType extends object | undefined> =
+    ObjectType extends object ?
     {
         [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
         ? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]> extends infer U extends string ? U : never}`
         : `${Key}`
-    }[keyof ObjectType & (string | number)];
+    }[keyof ObjectType & (string | number)] : never;
